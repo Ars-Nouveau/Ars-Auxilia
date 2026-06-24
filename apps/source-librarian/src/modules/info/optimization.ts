a8278323a6f22b9c990578119bb5a7884e3d92a0
Qther <qther@tuta.io>
2026-06-24 20:11:11 +0800

    feat: optimization info command

---
+++ apps/source-librarian/src/modules/info/optimization.ts
@@ -0,0 +1,45 @@
+import { type ChatInputCommandInteraction, type User } from "discord.js";
+import { Discord, Slash, SlashOption } from "discordx";
+
+import { InfoCommand } from "../../abstracts/info-command.js";
+import { userSlashOption } from "../../util/interaction.js";
+
+@Discord()
+export class OptimizationCommand extends InfoCommand {
+  description = "How do I make my game run faster?";
+  lines = [
+    "Optimization mods may affect the client (FPS), the server (TPS), or both. The ones listed here are compatible with most mods and aim to not affect gameplay.",
+    "Usage of OptiFine is discouraged as it tends to be incompatible with many mods, including Ars Nouveau.",
+  ];
+  fields = [
+    {
+      name: "Sodium (Client) [Curseforge](<https://www.curseforge.com/minecraft/mc-mods/sodium>) [Modrinth](<https://modrinth.com/mod/sodium>)",
+      value: "Can drastically improve frame-rates, compatible with shaders via Iris."
+    },
+    {
+      name: "Lithium (Both) [Curseforge](<https://www.curseforge.com/minecraft/mc-mods/lithium>) [Modrinth](<https://modrinth.com/mod/lithium>)",
+      value: "General-purpose optimization mod mostly focused on Vanilla systems."
+    },
+    {
+      name: "FerriteCore (Both) [Curseforge](<https://www.curseforge.com/minecraft/mc-mods/ferritecore>) [Modrinth](<https://modrinth.com/mod/ferrite-core>)",
+      value: "Reduces memory usage, can be quite significant in larger modpacks."
+    },
+    {
+      name: "ImmediatelyFast (Client) [Curseforge](<https://www.curseforge.com/minecraft/mc-mods/immediatelyfast>) [Modrinth](<https://modrinth.com/mod/immediatelyfast>)",
+      value: "Batches immediate mode rendering tasks to, often leading to notable improvements."
+    },
+    {
+      name: "ScalableLux (Both) [Curseforge](<https://www.curseforge.com/minecraft/mc-mods/scalablelux>) [Modrinth](<https://modrinth.com/mod/scalablelux>)",
+      value: "Replaces the light engine with a faster one, helps especially with chunk generation."
+    }
+  ];
+
+  @Slash({ name: "mana", description: "How do I get more mana?" })
+  override async run(
+    @SlashOption(userSlashOption)
+    user: User | undefined,
+    interaction: ChatInputCommandInteraction,
+  ) {
+    await super.run(user, interaction);
+  }
+}
