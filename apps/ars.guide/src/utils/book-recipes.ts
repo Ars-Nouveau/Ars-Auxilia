9fa9fee04af275448a2ca94efec96fd1eb9fb05c
Qther <qther@tuta.io>
2026-06-24 21:18:40 +0800

    cleanup

--- apps/ars.guide/src/utils/book-recipes.ts
+++ apps/ars.guide/src/utils/book-recipes.ts
@@ -6,13 +6,44 @@ export const isRecipeRecord = (value: unknown): value is BookRecipeRecord =>
 export const asRecipeRecord = (value: unknown) =>
   isRecipeRecord(value) ? value : undefined;
 
-export const asRecipeRecordArray = (value: unknown) =>
+export const asRecipeRecordArray = (value: unknown) => 
   Array.isArray(value) ? value.filter(isRecipeRecord) : [];
 
 export const asStringArray = (value: unknown) =>
   Array.isArray(value)
     ? value.filter((item): item is string => typeof item === "string")
     : [];
+
+export const asPedestalCells = (center: unknown, items: unknown[]): unknown[] => {
+  const cells = [
+    {},
+    {},
+    {},
+    {},
+    center,
+    {},
+    {},
+    {},
+    {},
+  ];
+
+  const cellIndices = [
+    [1],
+    [1, 7],
+    [1, 6, 8],
+    [1, 5, 7, 3],
+    [0, 1, 2, 6, 8],
+    [0, 1, 2, 6, 7, 8],
+  ]
+
+  let usedCells = cellIndices[items.length - 1] ?? [1, 2, 5, 8, 7, 6, 3, 0];
+
+  for (let i = 0; i < usedCells.length; i++) {
+    cells[usedCells[i]] = items[i];
+  }
+
+  return cells;
+}
 
 export const getRecipeResult = (recipe: BookRecipeRecord) =>
   asRecipeRecord(recipe.result) ?? asRecipeRecord(recipe.output);
